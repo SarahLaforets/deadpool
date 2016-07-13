049fe45284da3324320d356fd5039f5d550a1891
sarahlaforets <sarah.laforets@gmail.com>
2016-07-13 19:21:27 +0100

    Init project

---
+++ Deadpool/Deadpool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Deadpool
+//
+//  Created by Sarah LAFORETS on 13/07/2016.
+//  Copyright © 2016 Sarah LAFORETS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
